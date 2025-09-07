6b250c8c3f3a709cf7d28897b4348d7bf28e4686
herobaku <mammedovemin@gmail.com>
2025-09-08 02:35:37 +0400

    json 1.1

--- server.js
+++ server.js
@@ -2,17 +2,9 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-
-// Используйте порт, предоставляемый Render
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(PORT, () => {
-  console.log(`JSON Server запущен на порту ${PORT}`);
-});
-
-// Получение доступа к объекту данных
-const db = router.db;
-console.log('Данные:', db.getState());
+server.listen(port);
